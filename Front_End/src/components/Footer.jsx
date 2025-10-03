bdea684f9297b902541a87dda096ecb468b99d0b
Kevin P <50870709+Nirfs@users.noreply.github.com>
2025-10-03 17:12:46 +0200

    feat: composant footer

--- Front_End/src/components/Footer.jsx
+++ Front_End/src/components/Footer.jsx
@@ -1,3 +1,9 @@
+import '../styles/footer.scss'
+
 export function Footer(){
-    
+    return(
+    <footer>
+        <p>Tout le contenue appartient à Kevin Pieplu. Tout droits reservé</p>
+    </footer>
+    )
 }
