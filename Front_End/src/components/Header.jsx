c93996fb19aab67361cc9b415727aa20ccc6e3e2
Kevin P <50870709+Nirfs@users.noreply.github.com>
2025-10-03 07:50:44 +0200

    feat: composant header

--- Front_End/src/components/Header.jsx
+++ Front_End/src/components/Header.jsx
@@ -1,5 +1,19 @@
+//Librairie
+import { NavLink } from "react-router-dom"
+import { HashLink } from 'react-router-hash-link';
+//composant
+import { Button } from "./Button"
+//styles
+import '../styles/header.scss'
 export function Header(){
     return(
-        <h1>Home</h1>
+        <header>
+            <h1>logo</h1>
+            <nav>
+                <NavLink to={'/'}>Acceuil</NavLink>
+                <HashLink smooth to="/#travaux">Travaux</HashLink>
+                <NavLink to={'/'}>Connexion</NavLink>
+            </nav>
+        </header>
     )
 }
