b9cc14c63bd7415eb13c55aed4e805c8431ac309
Kevin P <50870709+Nirfs@users.noreply.github.com>
2025-10-03 17:15:17 +0200

    refactor:: header ajout du log

--- Front_End/src/components/Header.jsx
+++ Front_End/src/components/Header.jsx
@@ -3,12 +3,15 @@ import { NavLink } from "react-router-dom"
 import { HashLink } from 'react-router-hash-link';
 //composant
 import { Button } from "./Button"
+//assets
+import logo from '../assets/logo_animation.gif'
 //styles
 import '../styles/header.scss'
+
 export function Header(){
     return(
         <header>
-            <h1>logo</h1>
+            <img src={logo} alt='logo animée'/>
             <nav>
                 <NavLink to={'/'}>Acceuil</NavLink>
                 <HashLink smooth to="/#travaux">Travaux</HashLink>
