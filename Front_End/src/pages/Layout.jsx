9959610fb47ddb0a328c11fcc341435627468f3c
Kevin P <50870709+Nirfs@users.noreply.github.com>
2025-10-03 07:50:23 +0200

    feat: ajout scss et mise en place des style globaux

--- Front_End/src/pages/Layout.jsx
+++ Front_End/src/pages/Layout.jsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom';
 import { Header } from '../components/Header';
 import { Footer } from '../components/Footer';
 //styles
-
+import '../styles/abstract/global.scss'
 export function Layout(){ 
     return(
         <>
