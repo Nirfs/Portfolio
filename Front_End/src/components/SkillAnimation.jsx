ea86424ce7ed2229d7f16c3bedf0ff5e621c284a
Kevin P <50870709+Nirfs@users.noreply.github.com>
2025-10-04 10:32:53 +0200

    feat: composant skill avec ses animations au scroll

--- Front_End/src/components/SkillAnimation.jsx
+++ Front_End/src/components/SkillAnimation.jsx
@@ -7,6 +7,8 @@ import reactLogo from '../assets/react_logo.svg'
 import htmlLogo from '../assets/html_logo.svg'
 import cssLogo from '../assets/css_logo.svg'
 import front_end from '../assets/front_end.svg'
+import back_end from '../assets/back_end.svg'
+import design from '../assets/design.svg'
 //styles
 import '../styles/skillAnimation.scss'
 
@@ -28,7 +30,7 @@ export function SkillAnimation(){
                     }}
                 >
                 <h2>COMPÉTENCES</h2>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio officiis reiciendis deserunt libero, mollitia saepe ex temporibus incidunt at vitae. Modi rem neque eius saepe doloribus unde in quidem consequuntur!</p>
+                <p>Petit tour d’horizon de mes compétences, entre design et développement.</p>
             </motion.div>
             <div className="skill_card_container">
                 <motion.div
@@ -40,11 +42,12 @@ export function SkillAnimation(){
                     <SkillCard
                         className='front_end'
                         title='FRONT END'
-                        text= 'blabla blabla blabla blabla blabla blabla blabla blabla blabla blabla blabla '
+                        text= 'Grâce à ma formation OpenClassrooms en développement web, j’ai acquis des compétences dans les principaux langages et outils du front-end.'
                         sources={[
-                            { src: reactLogo, alt: "logo react" },
-                            { src: htmlLogo, alt: "html node" },
-                            { src: cssLogo, alt: "logo css" },
+                            { src: 'https://www.svgrepo.com/show/354259/react.svg', alt: "logo react" },
+                            { src: 'https://www.svgrepo.com/show/452228/html-5.svg', alt: "html node" },
+                            { src: 'https://www.svgrepo.com/show/452185/css-3.svg', alt: "logo css" },
+                            {src:'https://www.svgrepo.com/show/452045/js.svg', alt:'logo javascript'}
                         ]}
                         image_src={front_end}
                     />
@@ -57,13 +60,12 @@ export function SkillAnimation(){
                     <SkillCard
                         className='back_end'
                         title='BACK END'
-                        text= 'blabla blabla blabla blabla blabla blabla blabla blabla blabla blabla blabla '
+                        text= "J'ai également développer des compétences en back-end, notamment avec Node.js, Express et MongoDB, pour concevoir des applications web complètes."
                         sources={[
-                            { src: reactLogo, alt: "logo react" },
-                            { src: htmlLogo, alt: "html node" },
-                            { src: cssLogo, alt: "logo css" },
+                            { src: 'https://www.svgrepo.com/show/452075/node-js.svg', alt: "logo node" },
+                            { src: 'https://www.svgrepo.com/show/331488/mongodb.svg', alt: "logo mongoDb" },
                         ]}
-                        image_src={front_end}
+                        image_src={back_end}
                     />
                 </motion.div>
                 <motion.div
@@ -75,13 +77,14 @@ export function SkillAnimation(){
                     <SkillCard
                         className='design'
                         title='DESIGN GRAPHIQUE'
-                        text= 'blabla blabla blabla blabla blabla blabla blabla blabla blabla blabla blabla '
+                        text= 'Fort d’un BTS en design graphique et de plusieurs années d’expérience créative, j’apporte une sensibilité artistique que je mets aujourd’hui au service du développement web.'
                         sources={[
-                            { src: reactLogo, alt: "logo react" },
-                            { src: htmlLogo, alt: "html node" },
-                            { src: cssLogo, alt: "logo css" },
+                            { src: 'https://www.svgrepo.com/show/184132/adobe-photoshop.svg', alt: "logo photoshop" },
+                            { src: 'https://www.svgrepo.com/show/303184/adobe-illustrator-cc-logo.svg', alt: "logo illustrator" },
+                            { src: 'https://www.svgrepo.com/show/303190/after-effects-cc-logo.svg', alt: "logo after effect" },
+                            
                         ]}
-                        image_src={front_end}
+                        image_src={design}
                     />
                 </motion.div>
             </div>
