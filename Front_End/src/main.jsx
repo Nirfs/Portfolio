130c65ae666f471d8c9c1a7438a5dca1ba2dd34c
Kevin P <50870709+Nirfs@users.noreply.github.com>
2025-10-07 15:17:10 +0200

    feat: context d'authentification

--- Front_End/src/main.jsx
+++ Front_End/src/main.jsx
@@ -2,9 +2,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import AppRouter from './routes/AppRouter';
+import { AuthProvider } from './hook/authProvider';
+import Modal from "react-modal";
+
+Modal.setAppElement("#root");
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AppRouter />
+    <AuthProvider>
+      <AppRouter />
+    </AuthProvider>
   </StrictMode>,
 )
