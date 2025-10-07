4f3b0f45256ede96c197866f2d224eb6c59ecf58
Kevin P <50870709+Nirfs@users.noreply.github.com>
2025-10-07 15:22:10 +0200

    reafctor: ajustement du fetch

--- Front_End/src/api/fetch.js
+++ Front_End/src/api/fetch.js
@@ -1,7 +1,7 @@
 const API_URL = 'http://localhost:4000';
 
 export async function getWork() {
-    const rep = await fetch('http://localhost:4000/api/work')
+    const rep = await fetch(`${API_URL}/api/work`)
     if(!rep) throw new Error('Erreur lors du chargement des travaux')
         return rep.json();
 }
