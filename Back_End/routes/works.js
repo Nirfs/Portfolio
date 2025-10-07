b762f0f1b5cabd3d0e9b3a6bfa80413c00f579cb
Kevin P <50870709+Nirfs@users.noreply.github.com>
2025-10-07 15:14:29 +0200

    feat: mise en place des routes backend

---
+++ Back_End/routes/works.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+const workCtrl = require('../controllers/works');
+
+const { upload, imageOptimizer } = require('../middleware/multer-config');
+const auth = require('../middleware/auth');
+
+router.get('/', workCtrl.getAllWorks)
+router.get('/:id', workCtrl.getWork)
+
+router.post('/', auth, upload, imageOptimizer, workCtrl.createWork);
+
+router.delete('/:id',auth, workCtrl.deleteBook);
+
+module.exports = router;
