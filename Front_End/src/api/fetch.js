559492f8624c60a1d2d6ff2c27852bc77847e51f
Kevin P <50870709+Nirfs@users.noreply.github.com>
2025-10-07 15:18:59 +0200

    refactor: changement lien fetch vers base de donnée

--- Front_End/src/api/fetch.js
+++ Front_End/src/api/fetch.js
@@ -1,5 +1,8 @@
+const API_URL = 'http://localhost:4000';
+
 export async function getWork() {
-    const rep = await fetch('/data/works.json')
+    const rep = await fetch('http://localhost:4000/api/work')
     if(!rep) throw new Error('Erreur lors du chargement des travaux')
         return rep.json();
 }
+
