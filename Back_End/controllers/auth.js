06d3494f90ca93321370b5c978a4496d5228ea9e
Kevin P <50870709+Nirfs@users.noreply.github.com>
2025-10-07 15:15:48 +0200

    feat: logique de l'authentification

---
+++ Back_End/controllers/auth.js
@@ -0,0 +1,58 @@
+const User = require('../models/User')
+const bcrypt = require ('bcrypt')
+const jwt = require('jsonwebtoken')
+
+exports.signUp = (req, res) => {
+
+    const {email, password} = req.body;
+
+    //Verifier si le corps de la requete existe
+    if(!email) {
+        return res.status(400).json({message: 'Email obligatoire'})
+    }
+    if(!password) {
+        return res.status(400).json({message: 'Mot de passe obligatoire'})
+    }
+
+
+    //Hash du mdp
+    bcrypt.hash(req.body.password, 10)
+        .then(hash => {
+            const user = new User ({
+                email: req.body.email,
+                password: hash
+            })
+
+        user.save()
+            .then(() => res.status(201).json({message: 'utilisateur crée'}))
+            .catch((error) => res.status(400).json({error}))
+    })
+    .catch((error) => res.status(500).json({error}))
+}
+
+exports.login = (req, res) => {
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if(user === null){
+                res.status(401).json({message:'Paire login/mot de passe incorrecte'})
+            } else {
+                bcrypt.compare(req.body.password, user.password)
+                    .then(valid => {
+                        if(!valid){
+                            return res.status(401).json({message:'Paire login/mot de passe incorrecte'})
+                        }
+
+                        res.status(200).json({
+                            userId: user._id,
+                            token: jwt.sign(
+                                {userId: user._id},
+                                process.env.JWT_SECRET,
+                                {expiresIn:'24h'}
+                            )
+                        })
+                    })
+                    .catch((error) => res.status(500).json({error}))
+            }
+        })
+        .catch(error => res.status(500).json({error}))
+};
