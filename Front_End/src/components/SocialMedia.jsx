1dda3af02ebe2a1fc7df42b835416ca180b46e3b
Kevin P <50870709+Nirfs@users.noreply.github.com>
2025-10-09 11:02:13 +0200

    feat: composant Resaux sociaux

---
+++ Front_End/src/components/SocialMedia.jsx
@@ -0,0 +1,36 @@
+import mail from '../assets/plane.svg';
+import linkedin from '../assets/linkedin.svg';
+import gitHub from '../assets/github.svg';
+import '../styles/socialMedia.scss'; 
+
+export function SocialMedia() {
+ const socialMedia = [
+    { src: mail, alt: 'Envoyer un email', link: 'mailto:pieplu.kevin@gmail.com' },
+    { src: linkedin, alt: 'Profil LinkedIn', link: 'https://www.linkedin.com/in/kevin-pieplu/'},
+    { src: gitHub, alt: 'Profil gitHub', link: 'https://github.com/Nirfs?tab=repositories'}
+  ];
+
+    return (
+    <section className="social-media">
+        <h2>RESAUX SOCIAUX</h2>
+        <p>Pour me rejoindre sur mes resaux sociaux ou m'envoyer un mail n'hésite pas !</p>
+            <div>
+        {socialMedia.map((media, index) => (
+            <a
+            key={index}
+            href={media.link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="social-icon"
+            >
+            <img
+                src={media.src}
+                alt={media.alt}
+                loading="lazy"
+            />
+            </a>
+        ))}
+      </div>
+    </section>
+  );
+}
