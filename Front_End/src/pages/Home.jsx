a72fb2458159b3a99bc8c387fc68696712bcd4f0
Kevin P <50870709+Nirfs@users.noreply.github.com>
2025-10-09 11:06:47 +0200

    reafactor: changemetn de la home et ajout des composants

--- Front_End/src/pages/Home.jsx
+++ Front_End/src/pages/Home.jsx
@@ -1,20 +1,23 @@
 //librairie
 import { useLoaderData } from 'react-router-dom'
+import { useState } from 'react'
 //composant
 import { PresentationAnimation } from '../components/PresentationAnimation'
 import { SkillAnimation } from '../components/SkillAnimation'
 import { Card } from '../components/Card'
 import { AddProjectModal } from '../components/AddProjectModal'
+import { SocialMedia } from '../components/SocialMedia'
 //assets
 import bg from '../assets/doodle_illustration.svg'
 //styles
 import '../styles/home.scss'
-import { useState } from 'react'
+import { Timeline } from '../components/TimeLine'
 
 
 export function Home(){
     const worksList = useLoaderData()
     const [filter, setFilter] = useState("all")
+    
     console.log(worksList)
     const filterWork = worksList.filter(work =>
         filter === 'all' ? true : work.category === filter
@@ -50,14 +53,22 @@ export function Home(){
                  <div className="card_section">
                     {filterWork.map((work) => (
                         <Card 
-                        key={work._id} 
-                        navLink={`/work/${work._id}`}
-                        src={work.imageUrl} 
-                        name={work.title} 
+                            key={work._id}
+                            alt={`photo de ${work.title}`}
+                            navLink={`/work/${work._id}`}
+                            src={work.imageUrl} 
+                            name={work.title} 
                         />
                     ))}
                 </div>
+            </section >
+            <section className='timeline_section'>
+                <h2 id='travaux'>PARCOURS</h2>
+                <Timeline></Timeline>
             </section>
+            <div className='empty_container'></div>
+            <SocialMedia/>
+
         </> 
     )
 }
