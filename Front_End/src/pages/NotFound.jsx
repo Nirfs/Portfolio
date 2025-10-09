a3eac8ebef2019d7ab875545b78ecf45d06dc31e
Kevin P <50870709+Nirfs@users.noreply.github.com>
2025-10-09 11:06:22 +0200

    feat: page not found

--- Front_End/src/pages/NotFound.jsx
+++ Front_End/src/pages/NotFound.jsx
@@ -1,6 +1,17 @@
+//librairie
+import { Link } from 'react-router-dom'
+//style
+import '../styles/notFound.scss'
+
 export function NotFound(){
     return(
-        <p>NOT FOUND</p> 
+        <>
+            <div className="error_Container">
+                <h1> 404</h1>
+                <p>Oups! La page que vous demandez n'existe pas.</p>
+                <Link to="/">Retourner sur la page d’accueil</Link>
+            </div>
+        </>
     )
 
 }
